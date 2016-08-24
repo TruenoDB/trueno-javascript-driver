b7e3daeac5125d77097a503b392b434955dc2c92
Servio Palacios <servio@palacios.com>
2016-08-23 20:25:52 -0400

    [search] - fetch obtains results for G

---
+++ experiments/graph-fetch.js
@@ -0,0 +1,37 @@
+/**
+ * Created by: victor on 5/29/16.
+ * Source: .js
+ * Author: victor
+ * Description:
+ *
+ */
+
+const Trueno = require('../lib/trueno');
+
+/* Instantiate connection */
+
+let trueno = new Trueno({host: 'http://localhost', port: 8000, debug: true});
+
+trueno.connect((s)=> {
+
+  console.log('connected', s.id);
+
+  /* Create a new Graph */
+  let g = trueno.Graph();
+
+  /* Set label: very important */
+  g.setLabel('graphi');
+
+  let filter = g.filter()
+                .term('prop.version', '1');
+
+  /* persist g */
+  g.fetch('g',filter).then((result) => {
+    console.log("Info from Graph g fetched", result);
+  }, (error) => {
+    console.log("Error: Could not fetch Graph g info", error);
+  });
+
+}, (s)=> {
+  console.log('disconnected', s.id);
+})
