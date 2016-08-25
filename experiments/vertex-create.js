5387e6c1d7bf3fc61aca0e2afa390d265067717f
victor Santos Uceta <victor.uceta@gmail.com>
2016-08-24 23:08:30 -0400

    -Adding the edge vertices retreival example.

--- experiments/vertex-create.js
+++ experiments/vertex-create.js
@@ -26,16 +26,19 @@ trueno.connect((s)=> {
 
   let v1 = g.addVertex();
   let v2 = g.addVertex();
+  let v3 = g.addVertex();
 
   /* Set custom ids */
   v1.setId(1);
   v2.setId(2);
+  v3.setId(3);
 
   /* Adding properties and computed fields */
   v1.setProperty('name', 'pepe');
   v1.setProperty('gender', 'F');
   v2.setProperty('name', 'juan');
   v2.setComputed('pagerank', 'rank', 5);
+  v3.setProperty('name', 'Rick');
 
   /* persist v1 */
   v1.persist().then((result) => {
@@ -45,6 +48,12 @@ trueno.connect((s)=> {
   });
   /* persist v2 */
   v2.persist().then((result) => {
+    console.log('Vertex successfully created with id: ', result);
+  }, (error) => {
+    console.log('Vertex persistence error: ',error);
+  });
+  /* persist v3 */
+  v3.persist().then((result) => {
     console.log('Vertex successfully created with id: ', result);
   }, (error) => {
     console.log('Vertex persistence error: ',error);
