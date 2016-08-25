cd2dba39664de0ecd531380919fd17c3aa8fd0b2
Servio Palacios <servio@palacios.com>
2016-08-25 09:53:15 -0400

    [count] - count example

---
+++ experiments/graph-count.js
@@ -0,0 +1,37 @@
+/**
+ * Created by: victor on 5/29/16.
+ * Source: .js
+ * Author: Servio
+ * Description:
+ *
+ */
+
+const Trueno = require('../lib/trueno');
+
+/* Instantiate connection */
+
+let trueno = new Trueno({host: 'http://localhost', port: 8000, debug: true});
+
+trueno.connect((s)=> {
+
+  console.log('connected', s.id);
+
+  /* Create a new Graph */
+  let g = trueno.Graph();
+
+  /* Set label: very important */
+  g.setLabel('graphi');
+
+                /* Create a filter */
+  let filter = g.filter()
+                  .term('prop.name', 'pepe');
+                  
+  g.count('v',filter).then((result) => {
+    console.log("Info from Graph g counted", result);
+  }, (error) => {
+    console.log("Error: Could not count Graph g info", error);
+  });
+
+}, (s)=> {
+  console.log('disconnected', s.id);
+})
