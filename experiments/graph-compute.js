399bccf5624191e183ba1edfb9c8f148e212f216
Servio Palacios <servio@palacios.com>
2016-09-17 19:44:38 -0400

    [compute] - [class] - [graph-compute.js]

---
+++ experiments/graph-compute.js
@@ -0,0 +1,36 @@
+/**
+ * Created by: Servio on 2016.09.17.
+ * Source: .js
+ * Author: Servio
+ * Description:
+ *
+ */
+
+const Trueno = require('../lib/trueno');
+const Enums = require("../lib/core/data_structures/enums");
+
+/* Instantiate connection */
+
+let trueno = new Trueno({host: 'http://localhost', port: 8000, debug: false});
+
+trueno.connect((s)=> {
+
+  console.log('connected', s.id);
+
+  /* Create a new Graph */
+  let g = trueno.Graph();
+  g.setId(1);
+  g.setLabel("graphi");
+
+  let c = g.getCompute(Enums.algorithmType.DEPENDENCIES);
+
+  console.log('------------------------Compute-------------------------------');
+
+  /* Get the compute of the algorithm */
+  c.deploy().then((jobId)=> {
+    console.log('JobId: ', jobId);
+  });
+
+}, (s)=> {
+  console.log('disconnected', s.id);
+})
