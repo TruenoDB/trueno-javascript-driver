3b41507e24faa486320048ebbd175c08d6308880
Servio Palacios <servio@palacios.com>
2016-09-21 17:51:31 -0400

    [compute] - complete PR results for citations data set

--- experiments/graph-compute.js
+++ experiments/graph-compute.js
@@ -22,9 +22,22 @@ trueno.connect((s)=> {
   g.setId(1);
   g.setLabel("graphi");
 
-  let c = g.getCompute(Enums.algorithmType.DEPENDENCIES);
 
   console.log('------------------------Compute-------------------------------');
+  let c = g.getCompute();
+
+  c.setAlgorithm(Enums.algorithmType.PAGE_RANK);
+  let parameters = {
+      schema:   {string: "citations"},
+      vertices: {string: "vertices"},
+      edges:    {string: "edges"},
+      vertexId: {string: "id"},
+      source:   {string: "source"},
+      target:   {string: "target"},
+      TOL:      {string: 0.001},
+      alpha:    {string: 0.85}
+  };
+  c.setParameters(parameters);
 
   /* Get the compute of the algorithm */
   c.deploy().then((jobId)=> {
@@ -40,7 +53,7 @@ trueno.connect((s)=> {
              });
           }
        });
-    }, 10000);
+    }, 5000);
 
   });
 
