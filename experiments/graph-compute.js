59da6368da47f1ee35903b5eaa2f7f33161d7890
Servio Palacios <servio@palacios.com>
2017-02-13 14:42:34 -0500

    [graph-compute] - [experiments] - script to test Trueno Compute - PakeRank Algorithm - Time is thrown in console

--- experiments/graph-compute.js
+++ experiments/graph-compute.js
@@ -1,62 +1,83 @@
 /**
- * Created by: Servio on 2016.09.17.
- * Source: .js
- * Author: Servio Palacios
- * Description:
+ * graph-compute.js
+ * This file connects to neo4j and retrieve pakeranks
  *
+ * @version 0.0.1
+ * @author   maverick-zhn(Servio Palacios)
+ * @updated 2017.02.13
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ * Do NOT forget to reference the ORIGINAL author of the code. Be nice!
  */
 
 const Trueno = require('../lib/trueno');
 const Enums = require("../lib/core/data_structures/enums");
 
+let requiredArguments = 3;
+let totalArguments = process.argv.length;
+
 /* Instantiate connection */
 
-let trueno = new Trueno({host: 'http://localhost', port: 8000, debug: false});
+if( !(totalArguments==requiredArguments) ) {
+  console.log("[usage] node graph-compute.js graph");
+}
+else {
 
-trueno.connect((s)=> {
+  const destinationGraph = process.argv[3];
 
-  console.log('connected', s.id);
+  let trueno = new Trueno({host: 'http://localhost', port: 8000, debug: false});
 
-  /* Create a new Graph */
-  let g = trueno.Graph();
-  g.setId(1);
-  g.setLabel("graphii");
+  console.time("TruenoConnect");
+  trueno.connect((s) => {
 
-  console.log('------------------------Compute-------------------------------');
-  let c = g.getCompute();
+    console.log('connected', s.id);
 
-  c.setAlgorithm(Enums.algorithmType.PAGE_RANK);
-  let parameters = {
-      schema:         {string: "graphii"},
-      tolerance:      {string: 0.001},
-      alpha:          {string: 0.85},
-      persisted:      {string: "false"},
+    /* Create a new Graph */
+    let g = trueno.Graph();
+    g.setId(1);
+    g.setLabel(destinationGraph);
+
+    console.log('------------------------Compute-------------------------------');
+    let c = g.getCompute();
+
+    c.setAlgorithm(Enums.algorithmType.PAGE_RANK);
+    let parameters = {
+      schema: {string: destinationGraph},
+      tolerance: {string: 0.001},
+      alpha: {string: 0.85},
+      persisted: {string: "false"},
       persistedTable: {string: "vertices"}
-  };
-  c.setParameters(parameters);
+    };
+    c.setParameters(parameters);
 
-  /* Get the compute of the algorithm */
-  c.deploy().then((jobId)=> {
-    console.log('JobId: ', jobId);
+    /* Get the compute of the algorithm */
+    c.deploy().then((jobId) => {
 
-     var x = setInterval(function () {
-       c.jobStatus(jobId).then((status)=> {
+      console.time("TruenoConnect");
+
+      console.log('JobId: ', jobId);
+
+      let x = setInterval(function () {
+        c.jobStatus(jobId).then((status) => {
           console.log('Job Status: ', status);
           if (status == Enums.jobStatus.FINISHED) {
-             c.jobResult(jobId).then((result)=> {
-               console.log('Job Result: ', result);
-               // var ranks = result.result;
-               // ranks.sort(function(a, b){return b[1]-a[1]});
-               // ranks.slice(1,10);
-               // console.log(ranks.slice(1,10));
-               clearInterval(x);
-             });
+            c.jobResult(jobId).then((result) => {
+              console.log('Job Result: ', result);
+              // var ranks = result.result;
+              // ranks.sort(function(a, b){return b[1]-a[1]});
+              // ranks.slice(1,10);
+              // console.log(ranks.slice(1,10));
+              clearInterval(x);
+            });
           }
-       });
-    }, 5000);
+        });
+      }, 5000);
 
+    });
+
+  }, (s) => {
+    console.log('disconnected', s.id);
   });
 
-}, (s)=> {
-  console.log('disconnected', s.id);
-})
+}
