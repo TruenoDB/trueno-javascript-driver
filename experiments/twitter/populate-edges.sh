a18dcf43d02db2a10c26a8e121b96e84e6601e23
ebarsallo <edgardo.barsallo@gmail.com>
2017-07-05 20:27:48 -0400

    Fix incremental

--- experiments/twitter/populate-edges.sh
+++ experiments/twitter/populate-edges.sh
@@ -4,8 +4,17 @@
 # Populate edges into twitter graph database (https://snap.stanford.edu/data/twitter-2010.html),
 # Data must be already converted in json files 
 
-offset=0
-inc=2000000
+#offset=0   # 0 (pdsl??)
+#offset=200000000   # 1 (pdsl??)
+#offset=400000000   # 2 (pdsl??)
+#offset=600000000   # 3 (pdsl??)
+#offset=800000000   # 4 (pdsl??)
+#offset=1000000000  # 5 (pdsl??)
+#offset=1200000000  # 6 (pdsl??)
+#offset=1400000000  # 7 (pdsl14)
+
+offset=1400000000
+inc=200000
 
 # edges 
 function edges() {
